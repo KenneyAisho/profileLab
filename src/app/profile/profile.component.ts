457215b81cba5aa69b7390cbcddce798062d089e
kenneyaisho <kenneyaisho@gmail.com>
2019-11-15 12:38:32 -0500

    imported router in my profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../service/profile.service';
 import { UserProfile } from '../user-profile';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,7 +11,7 @@ import { UserProfile } from '../user-profile';
 export class ProfileComponent implements OnInit {
   profile: UserProfile;
 
-  constructor(private service: ProfileService) {}
+  constructor(private service: ProfileService, private router: Router) {}
 
   ngOnInit() {
     this.profile = this.service.getUserProfile();
